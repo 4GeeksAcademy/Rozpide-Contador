71868efe6014839df8207f59391abc17985b85b4
Rozpi <100348581+Rozpide@users.noreply.github.com>
2024-12-18 00:12:36 +0000

    creo el contador

---
+++ src/js/components/ContadorSegundos.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ContadorSegundos.css'; // importo el archivo css para las clssName
+// funcion ContadorSegundos que recibe la propiedad segundos
+const ContadorSegundos = ({ segundos }) => {
+    // funcion para formatear segundos, que convierte a string el numero de segundos
+  const formatearSegundos = (segundos) => {
+    //padStart hace que el string siempre tenga 6 digitos rellenando con 0 a la izda
+    const str = segundos.toString().padStart(6, '0');
+    //split divide el string en un array de caracteres
+    return str.split('');
+  };
+// obtenemos los digitos llamando la funcion formatearSegundos, a traves de la prop segundos
+  const digitos = formatearSegundos(segundos);
+
+  return (
+    <div className="contador">
+      <div className="icono-reloj">
+        <i className="fa fa-clock-o"></i>
+      </div>
+      {digitos.map((digito, indice) => (
+        <div key={indice} className="digito">
+          {digito}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+ContadorSegundos.propTypes = {
+  segundos: PropTypes.number
+};
+
+export default ContadorSegundos;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import ContadorSegundos from './ContadorSegundos';
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [segundos, setSegundos] = useState(0);
+  const [estaCorriendo, setEstaCorriendo] = useState(true);
+  const [tiempoAlerta, setTiempoAlerta] = useState(null); // Estado para el tiempo de alerta
+  const intervaloRef = useRef(null);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    if (estaCorriendo) {
+      intervaloRef.current = setInterval(() => {
+        setSegundos((prevSegundos) => prevSegundos + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervaloRef.current);
+    }
+    return () => clearInterval(intervaloRef.current);
+  }, [estaCorriendo]);
+
+  useEffect(() => {
+    if (tiempoAlerta !== null && segundos === tiempoAlerta) {
+      alert(`Tiempo alcanzado: ${tiempoAlerta} segundos`);
+    }
+  }, [segundos, tiempoAlerta]);
+
+  const pararContador = () => setEstaCorriendo(false);
+  const reiniciarContador = () => {
+    setEstaCorriendo(false);
+    setSegundos(0);
+    setEstaCorriendo(true);
+  };
+  const resumirContador = () => setEstaCorriendo(true);
+  const manejarCambioAlerta = (e) => setTiempoAlerta(Number(e.target.value));
+
+  return (
+    <div className="text-center">
+      <ContadorSegundos segundos={segundos} />
+      <div className="btn-group mt-3" role="group">
+        <button className="btn btn-danger" onClick={pararContador}>Parar</button>
+        <button className="btn btn-warning" onClick={reiniciarContador}>Reiniciar</button>
+        <button className="btn btn-primary" onClick={resumirContador}>Resumir</button>
+      </div>
+      <div className="mt-3">
+        <label htmlFor="tiempoAlerta">Tiempo para la alerta (segundos): </label>
+        <input type="number" id="tiempoAlerta" onChange={manejarCambioAlerta} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
